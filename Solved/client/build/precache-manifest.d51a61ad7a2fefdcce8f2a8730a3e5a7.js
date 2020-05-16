ba10aef6b95843654ef250a7a5aabcfcd9ab43ce
Brian Cancel <brian.e.cancel@gmail.com>
2020-05-15 21:08:05 -0400

    project push

---
+++ Solved/client/build/precache-manifest.d51a61ad7a2fefdcce8f2a8730a3e5a7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec66eff279fa3cc7e9a7",
+    "url": "/static/js/main.cb7a2cd7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c4db9f5f82ebd071105f",
+    "url": "/static/js/2.ab7a91be.chunk.js"
+  },
+  {
+    "revision": "c95c4777880ea9f90cb5fb530539a304",
+    "url": "/index.html"
+  }
+];
--- Solved/client/build/service-worker.js
+++ Solved/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f34ed17516986276c0e53edfcdbb5c3.js"
+  "/precache-manifest.d51a61ad7a2fefdcce8f2a8730a3e5a7.js"
 );
 
 workbox.clientsClaim();
